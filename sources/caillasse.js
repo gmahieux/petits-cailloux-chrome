b79e8d0ce83c52849e6e3ab8f8b8ef4e12766752
gmahieux <ghislain.mahieux@free.fr>
2013-06-08 14:22:09 +0200

    Ajout d'un raccourci pour charger les infos
    Ctrl+F9 permet de charger les infos petits-cailloux dans le cas où on
    modifie les critères de recherche sur la page de resultats.
    Sans celà les infos n'apparaisent pas car les données sont chargées via un
    appel ajax

--- sources/caillasse.js
+++ sources/caillasse.js
@@ -2,9 +2,18 @@ if ($(".action_print_ann").length>0) { // detail d'une annonce
 	var idAnnonce = $(".action_print_ann").attr("rel").split("/")[2];
 	$(".read_more").after("<img src='http://petitscailloux.com/Graph.aspx?bFrame=true&sId="+idAnnonce+"'/>");
 } else if ($(".acc_detail").length>0) { //resultat de recherche
+	loadPetitsCailloux();
+}
+function loadPetitsCailloux() {
 	$(".acc_detail").each(function (index) {
 		url =$("a",this).attr("href");
 		/.*\/(.*?)\.htm/.exec(url);
 		$(this).after("<img src='http://petitscailloux.com/Graph.aspx?bFrame=true&sId="+ RegExp.$1 +"'/>");
 	});
 }
+$(document).keydown(function(event) {
+	console.log(event);
+	if (event.keyCode == 120 && event.ctrlKey == true) {//ctrl + F9
+		loadPetitsCailloux();
+	}
+})
